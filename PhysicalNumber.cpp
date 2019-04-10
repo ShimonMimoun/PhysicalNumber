5306f4ed4375e2f1388f57c0fa9beb65016fa4c5
ShimonMimoun <shimon1705@gmail.com>
2019-04-10 19:54:43 +0300

    fixfub 3

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -79,39 +79,27 @@ const PhysicalNumber ariel::PhysicalNumber::operator-() {
 bool ariel::operator== (const PhysicalNumber& phy, const PhysicalNumber& phy2){
     if(!checkType(phy.unit_type, phy2.unit_type)){
          throw std::invalid_argument("you cant using operation '==' with differnt types.");
-    }
-
-    return (double_base(phy.value_number, phy.unit_type)) == (double_base(phy2.value_number, phy2.unit_type));
+    }  return (double_base(phy.value_number, phy.unit_type)) == (double_base(phy2.value_number, phy2.unit_type));
 };
 
 
 bool ariel::operator> (const PhysicalNumber& phy, const PhysicalNumber& phy2){
     if(!checkType(phy.unit_type, phy2.unit_type)){
          throw std::invalid_argument("you cant using operation '>' with differnt types.");
-    }
-        double value_number1 = double_base(phy.value_number, phy.unit_type);
-    double value_number2 = double_base(phy2.value_number, phy2.unit_type);
-    return value_number1 > value_number2;
+    }  return (double_base(phy.value_number, phy.unit_type)) > (double_base(phy2.value_number, phy2.unit_type));
 };
 
 
 bool ariel::operator< (const PhysicalNumber& phy, const PhysicalNumber& phy2){
     if(!checkType(phy.unit_type, phy2.unit_type)){
         throw std::out_of_range{"Error: Numbers with differents type..."};
-    }
-
-    double value_number1 = double_base(phy.value_number, phy.unit_type);
-    double value_number2 = double_base(phy2.value_number, phy2.unit_type);
-    return value_number1 < value_number2;
+    }return (double_base(phy.value_number, phy.unit_type)) < (double_base(phy2.value_number, phy2.unit_type));
 };
 
 bool ariel::operator>= (const PhysicalNumber& phy, const PhysicalNumber& phy2){
     if(!checkType(phy.unit_type, phy2.unit_type)){
          throw std::invalid_argument("you cant using operation '>=' with differnt types.");
-    }
-
-
-    return (double_base(phy.value_number, phy.unit_type)) >= (double_base(phy2.value_number, phy2.unit_type));
+    } return (double_base(phy.value_number, phy.unit_type)) >= (double_base(phy2.value_number, phy2.unit_type));
 };
 
 
@@ -182,55 +170,81 @@ ostream& ariel::operator<< (ostream& os, const PhysicalNumber& p_temp) {
 };
 
 
+istream& ariel::operator>> (istream& input, PhysicalNumber& other){
+    int data_number;
+    string unit;
+    Unit u;
 
+   
+    ostringstream os;       
+    input >> os.rdbuf();    
+    string str = os.str();  
+    int unitSize = verif_string_unit(str); 
+int data_numberSize = data_numberCorrect(str); 
 
+  
+    ios::pos_type startPosition = input.tellg();
 
-Unit get_Unit(int unit){
-    Unit temp;
-    switch (unit)
-    {
-        case 1: temp = Unit::CM;
-            break;
-        case 2: temp = Unit::M;
-            break;
-        case 3: temp = Unit::KM;
-            break;
-        case 4: temp = Unit::SEC;
-            break;
-        case 5: temp = Unit::MIN;
-            break;
-        case 6: temp = Unit::HOUR;
-            break;
-        case 7: temp = Unit::G;
-            break;
-        case 8: temp = Unit::KG;
-            break;    
-        case 9: temp = Unit::TON;
-            break;
+    if (!unitSize || !data_numberSize) { 
+      
+        auto errorState = input.rdstate();
+        input.clear(); 
+        input.seekg(startPosition); 
+        input.clear(errorState);
+
+    } else {
+        other.value_number = getdata_number(str);
+        other.unit_type = get_Unit(unitSize);
     }
-    return temp;
-} 
+
+    return input;
+};
+
+
+
+
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//                          HELP FONCTION
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
 
 
 int verif_string_unit(string str){
-    int unitSize = size_uni(str);
-    int unit = 0;
-    if(unitSize == 0 || unitSize > 4){
+    int result = 0;
+    int size = size_uni(str);
+  
+    if(size == 0 || size > 4){
         return 0;
     }
-    else{
-        string tempUnit = str.substr(str.length() - unitSize - 1, unitSize);
-        if(!tempUnit.compare("cm")){ unit = (int)Unit::CM; }
-        if(!tempUnit.compare("m")){ unit = (int)Unit::M; }
-        if(!tempUnit.compare("km")){ unit = (int)Unit::KM; }
-        if(!tempUnit.compare("sec")){ unit = (int)Unit::SEC; }
-        if(!tempUnit.compare("min")){ unit = (int)Unit::MIN; }
-        if(!tempUnit.compare("hour")){ unit = (int)Unit::HOUR; }
-        if(!tempUnit.compare("g")){ unit = (int)Unit::G; }
-        if(!tempUnit.compare("kg")){ unit = (int)Unit::KG; }
-        if(!tempUnit.compare("ton")){ unit = (int)Unit::TON; }
+    else
+    {
+        string str_unit = str.substr(str.length()-1-size, size);
+          if(!str_unit.compare("g")){ result = (int)Unit::G; }
+        if(!str_unit.compare("kg")){ result = (int)Unit::KG; }
+        if(!str_unit.compare("ton")){ result = (int)Unit::TON; }
+        if(!str_unit.compare("cm")){ result = (int)Unit::CM; }
+        if(!str_unit.compare("m")){ result = (int)Unit::M; }
+        if(!str_unit.compare("km")){ result = (int)Unit::KM; }
+        if(!str_unit.compare("sec")){ result = (int)Unit::SEC; }
+        if(!str_unit.compare("min")){ result = (int)Unit::MIN; }
+        if(!str_unit.compare("hour")){ result = (int)Unit::HOUR; }
+      
     }
-    return unit;
+    return result;
 }
 
 
@@ -266,36 +280,6 @@ double getdata_number(string str){
     return data_number;
 }
 
-
-istream& ariel::operator>> (istream& input, PhysicalNumber& other){
-    int data_number;
-    string unit;
-    Unit u;
-
-   
-    ostringstream os;       
-    input >> os.rdbuf();    
-    string str = os.str();  
-    int unitSize = verif_string_unit(str); 
-int data_numberSize = data_numberCorrect(str); 
-
-  
-    ios::pos_type startPosition = input.tellg();
-
-    if (!unitSize || !data_numberSize) { 
-      
-        auto errorState = input.rdstate();
-        input.clear(); 
-        input.seekg(startPosition); 
-        input.clear(errorState);
-
-    } else {
-        other.value_number = getdata_number(str);
-        other.unit_type = get_Unit(unitSize);
-    }
-
-    return input;
-};
 
 bool checkType (Unit unit_a, Unit unit_b){
     int this_type = ((int)unit_a - 1) / 3;
@@ -375,3 +359,29 @@ int size_uni(string str){
 
     return unit_size - 1;
 }
+
+Unit get_Unit(int unit){
+    Unit temp;
+    switch (unit)
+    {
+        case 1: temp = Unit::CM;
+            break;
+        case 2: temp = Unit::M;
+            break;
+        case 3: temp = Unit::KM;
+            break;
+        case 4: temp = Unit::SEC;
+            break;
+        case 5: temp = Unit::MIN;
+            break;
+        case 6: temp = Unit::HOUR;
+            break;
+        case 7: temp = Unit::G;
+            break;
+        case 8: temp = Unit::KG;
+            break;    
+        case 9: temp = Unit::TON;
+            break;
+    }
+    return temp;
+} 
